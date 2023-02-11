58bb626ed75d41f539fd001c64d0e355f28ffad0
Rolf Erik Lekang <me@rolflekang.com>
2023-02-11 19:34:30 +0100

    Add dd for diagnostics

--- nvim/init.lua
+++ nvim/init.lua
@@ -272,6 +272,7 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
 
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
