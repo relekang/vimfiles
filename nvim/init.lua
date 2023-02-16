bf7212f3bb631ef923f09854bda506e5ebedbab2
Rolf Erik Lekang <me@rolflekang.com>
2023-02-16 20:26:32 +0100

    Add null-ls

--- nvim/init.lua
+++ nvim/init.lua
@@ -58,6 +58,7 @@ require('lazy').setup({
 
   'christoomey/vim-tmux-navigator',
   'editorconfig/editorconfig-vim',
+  'jose-elias-alvarez/null-ls.nvim',
 
   'github/copilot.vim',
 
@@ -370,6 +371,10 @@ vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
 vim.keymap.set('n', '+', function() require('neo-tree').show('', true) end, {})
+
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+
+
 -- Turn on lsp status information
 require('fidget').setup()
 
@@ -442,5 +447,28 @@ update_color_mode()
 
 vim.keymap.set('n', '<leader>cm', update_color_mode, { desc = 'Update colorscheme based on color-mode' })
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local null_ls = require('null-ls')
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.formatting.eslint,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.isort,
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
+})
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
