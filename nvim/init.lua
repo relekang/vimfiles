c812f36b13a9c03b4a8698d7adcd78f77fee492b
Rolf Erik Lekang <me@rolflekang.com>
2023-09-25 22:02:02 +0200

    feat: Initial go setup in neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -88,6 +88,20 @@ require('lazy').setup({
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
     }
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   }
 })
 
