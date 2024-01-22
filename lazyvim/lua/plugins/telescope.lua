2ae439a6a7865f756264efe05766696eccec63f4
Rolf Erik Lekang <me@rolflekang.com>
2024-01-22 15:19:45 +0100

    Add keymap for search for conflicts

--- lazyvim/lua/plugins/telescope.lua
+++ lazyvim/lua/plugins/telescope.lua
@@ -4,5 +4,6 @@ return {
   "nvim-telescope/telescope.nvim",
   keys = {
     { "<leader><space>", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
+    { "<leader>gx", "<cmd>Telescope grep_string search=<<<< <CR>" },
   },
 }
