cd3432c04ce22d3dee2b8fe574085b37690e4f0d
Rolf Erik Lekang <me@rolflekang.com>
2024-01-22 15:20:21 +0100

    fix: Update plugin config

---
+++ lazyvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    background = {
+      light = "latte",
+      dark = "mocha",
+    },
+  },
+}
