047d6e0330c2a17f6f4303191a8867c2da21bce4
Rolf Erik Lekang <me@rolflekang.com>
2024-04-15 10:11:51 +0200

    fix: Remove typos

--- lazyvim/after/queries/rust/injections.scm
+++ lazyvim/after/queries/rust/injections.scm
@@ -8,7 +8,7 @@
   (token_tree (string_literal) @injection.content
     (#offset! @injection.content 0 1 0 -1)
     (#set! injection.language "sql") 
-    (#set! injeciton.include-children true)
+    (#set! injection.include-children true)
   )
 )
 
@@ -19,6 +19,6 @@
   (token_tree (string_literal) @injection.content
     (#offset! @injection.content 0 1 0 -1)
     (#set! injection.language "sql") 
-    (#set! injeciton.include-children true)
+    (#set! injection.include-children true)
   )
 )
