a1c13e274e99c508c2145ae6289fe14a8b6d3abf
Rolf Erik Lekang <me@rolflekang.com>
2025-09-19 08:53:53 +0200

    Add mason config

---
+++ lazyvim/lua/plugins/mason.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    version = "2.0.0",
+    opts = {
+      ensure_installed = {
+        "basedpyright",
+        "json-lsp",
+        "lua-language-server",
+        "prettier",
+        "ruff",
+        "rust-analyzer",
+        "typescript-language-server",
+      },
+    },
+  },
+  { "mason-org/mason-lspconfig.nvim" },
+}
