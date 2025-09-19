f60dd3704163027824c6f83831a85f26ba6f42fc
Rolf Erik Lekang <me@rolflekang.com>
2025-09-19 08:52:21 +0200

    Add git keymaps

--- lazyvim/lua/plugins/telescope.lua
+++ lazyvim/lua/plugins/telescope.lua
@@ -17,5 +17,38 @@ return {
       end,
       desc = "Grep <<< string",
     },
+    -- Git branch selection
+    {
+      "<leader>gb",
+      function()
+        telescope_builtin.git_branches({
+          attach_mappings = function(_, map)
+            map("i", "<CR>", function(prompt_bufnr)
+              local selection = require("telescope.actions.state").get_selected_entry()
+              require("telescope.actions").close(prompt_bufnr)
+              vim.cmd("Git checkout " .. selection.value)
+            end)
+            return true
+          end,
+        })
+      end,
+      desc = "Git branches",
+    },
+    -- Git status
+    {
+      "<leader>gs",
+      function()
+        telescope_builtin.git_status()
+      end,
+      desc = "Git status",
+    },
+    -- Git commits
+    {
+      "<leader>gc",
+      function()
+        telescope_builtin.git_commits()
+      end,
+      desc = "Git commits",
+    },
   },
 }
