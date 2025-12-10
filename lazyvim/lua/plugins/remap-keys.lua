911ae9bc57ff98638bb10b5339c48c3249bc8125
Rolf Erik Lekang <me@rolflekang.com>
2025-12-10 14:38:27 +0100

    fix: st keybinding

--- lazyvim/lua/plugins/remap-keys.lua
+++ lazyvim/lua/plugins/remap-keys.lua
@@ -1,13 +1,15 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      local Keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- stylua: ignore
-      vim.list_extend(Keys, {
-        { "<leader>st", function() Snacks.picker.lsp_workspace_symbols({ filter = LazyVim.config.kind_filter }) end, desc = "LSP Workspace Symbols", has = "workspace/symbols" },
-      })
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "<leader>st", function() Snacks.picker.lsp_workspace_symbols({ filter = LazyVim.config.kind_filter }) end, desc = "LSP Workspace Symbols", has = "workspace/symbols" },
+          },
+        },
+      },
+    },
   },
   {
     "folke/todo-comments.nvim",
